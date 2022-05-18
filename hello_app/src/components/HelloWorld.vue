d88b2c9d4a02ba9d20ff17254890fe62349a93c6
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-05-18 23:04:26 +0900

    [Finish]Hello_app

---
+++ hello_app/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <div>
+      <input type="text" v-model="input">
+      <button v-on:click="doAction">Click</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+  },
+  data:function() {
+    return{
+      message: 'お名前は？',
+      input: 'no name',
+    };
+  },
+  methods: {
+    doAction:function(){
+      this.message = 'こんにちは' + this.input + 'さん';
+      this.$emit('result-event', this.input);
+    }
+  }
+}
+</script>
+
+<style>
+div {
+  margin: 0;
+  padding: 0;
+  text-align: left;
+}
+
+h1 {
+  font-size: 72px;
+  font-weight: bold;
+  text-align: right;
+  letter-spacing: 8px;
+  color: #f0f0f0;
+  margin: 0;
+}
+
+p {
+  margin: 0;
+  color: #666;
+  font-size: 16px;
+}
+</style>
+
